d5e2556753a9340aff7cf55af2cc4056abf8f95b
KajalBansal25 <kajal.bansal@daffodilsw.com>
2022-10-19 11:37:25 +0530

    showing userdata to admin screen

--- src/components/screens/admin_screen.js
+++ src/components/screens/admin_screen.js
@@ -1,23 +1,66 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
 export default function AdminScreen({setIsLoggedin}) {
+  const [dataForAdmin, setDataForAdmin] = useState([]);
+
+  useEffect(() => {
+    fetchDataForAdmin();
+  }, []);
+
+  function fetchDataForAdmin() {
+    axios
+      .get('https://dansir-backend.herokuapp.com/api/v1/find_data/find')
+      .then(response => {
+        setDataForAdmin(response.data.result);
+        console.log('response in admin screen>>>>>>', response.data.result);
+      });
+  }
+
   return (
     <View>
-      <Text>Admin Screen</Text>
-      <Button
-        title="logout"
-        onPress={async () => {
-          try {
-            await AsyncStorage.removeItem('@storage_Key');
-            setIsLoggedin(false);
-          } catch (e) {
-            console.log('error in logout>>>', e);
-          }
-          console.log('Done.');
-        }}
-      />
+      <Text style={{fontSize: 20, justifyContent: 'center'}}>Admin Screen</Text>
+      {console.log('dataForAdmin>>>>>', dataForAdmin)}
+      {dataForAdmin.map((data, i) => {
+        console.log('data4567890987654345678', data.employee_name);
+        return (
+          <View key={i}>
+            <Text style={styles.textInput}>
+              Employee name : {data.employee_name}{' '}
+            </Text>
+            <Text style={styles.textInput}>
+              employee code : {data.employee_code}
+            </Text>
+            <Text style={styles.textInput}>
+              total amount to pay : {data.totalAmount}{' '}
+            </Text>
+            <Text style={styles.textInput}>actions : </Text>
+            <View style={{margin: 10}}>
+              <Button title="Pay" onPress={() => {}} />
+            </View>
+          </View>
+        );
+      })}
+      <View style={{margin: 10}}>
+        <Button
+          title="logout"
+          onPress={async () => {
+            try {
+              await AsyncStorage.removeItem('@storage_Key');
+              setIsLoggedin(false);
+            } catch (e) {
+              console.log('error in logout>>>', e);
+            }
+            console.log('Done.');
+          }}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textInput: {backgroundColor: 'yellow', flexDirection: 'row'},
+});
---
+++ src/service.js
@@ -0,0 +1,181 @@
+import axios from 'axios';
+
+export function getHeader() {
+  let header = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
+  return header;
+}
+
+export function createAccountApi(payload, successCallback, failCallback) {
+  return axios
+    .post(`${process.env.REACT_APP_CONFIG_URL}/api/v1/create_user`, payload, {
+      headers: getHeader(),
+    })
+    .then(resp => {
+      console.log('resp====>>>>', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>', ex);
+      failCallback(ex);
+    });
+}
+
+export function logInApi(payload, successCallback, failCallback) {
+  return axios
+    .post(`${process.env.REACT_APP_CONFIG_URL}/api/v1/logIn`, payload, {
+      headers: getHeader(),
+    })
+    .then(resp => {
+      console.log('resp====>>>>', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>', ex);
+      failCallback(ex);
+    });
+}
+
+export function verifyEmailOfUser(payload, successCallback, failCallback) {
+  return axios
+    .post(`${process.env.REACT_APP_CONFIG_URL}/api/v1/verify_email`, payload, {
+      headers: getHeader(),
+    })
+    .then(resp => {
+      console.log('resp====>>>>', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>', ex);
+      failCallback(ex);
+    });
+}
+
+export function getUserData(payload, successCallback, failCallback) {
+  return axios
+    .post(`${process.env.REACT_APP_CONFIG_URL}/api/v1/get_user_data`, payload, {
+      headers: getHeader(),
+    })
+    .then(resp => {
+      console.log('resp====>>>>', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>', ex);
+      failCallback(ex);
+    });
+}
+
+export function getTotalAmountOfUser(payload, successCallback, failCallback) {
+  return axios
+    .post(
+      `${process.env.REACT_APP_CONFIG_URL}/api/v1/get_Total_user`,
+      payload,
+      {
+        headers: getHeader(),
+      },
+    )
+    .then(resp => {
+      console.log('resp====>>>>111', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>', ex);
+      failCallback(ex);
+    });
+}
+
+export function sendAmoundOfPurchased(payload, successCallback, failCallback) {
+  return axios
+    .post(
+      `${process.env.REACT_APP_CONFIG_URL}/api/v1/amount/amount_user`,
+      payload,
+      {
+        headers: getHeader(),
+      },
+    )
+    .then(resp => {
+      console.log('resp====>>>>111', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>', ex);
+      failCallback(ex);
+    });
+}
+
+export function showAllAdminData(successCallback, failCallback) {
+  return axios
+    .get(`${process.env.REACT_APP_CONFIG_URL}/api/v1/find_data/find`)
+    .then(resp => {
+      console.log('resp====>>>>111', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>', ex);
+      failCallback(ex);
+    });
+}
+
+export function getMoneyFromUser(payload, successCallback, failCallback) {
+  return axios
+    .post(
+      `${process.env.REACT_APP_CONFIG_URL}/api/v1/money/get_money`,
+      payload,
+      {
+        headers: getHeader(),
+      },
+    )
+    .then(resp => {
+      console.log('resp====>>>>111', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>Paymoney', ex);
+      failCallback(ex);
+    });
+}
+
+export function forgetPasswordApi(payload, successCallback, failCallback) {
+  return axios
+    .post(
+      `${process.env.REACT_APP_CONFIG_URL}/api/v1/forget_password/sendEmail`,
+      payload,
+      {
+        headers: getHeader(),
+      },
+    )
+    .then(resp => {
+      console.log('resp====>>>>111', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>Paymoney', ex);
+      failCallback(ex);
+    });
+}
+
+export function forgetPasswordApiAfterVarify(
+  payload,
+  successCallback,
+  failCallback,
+) {
+  return axios
+    .post(
+      `${process.env.REACT_APP_CONFIG_URL}/api/v1/forget_password`,
+      payload,
+      {
+        headers: getHeader(),
+      },
+    )
+    .then(resp => {
+      console.log('resp====>>>>111', resp);
+      successCallback(resp);
+    })
+    .catch(ex => {
+      console.log('errrr--->>>>Paymoney', ex);
+      failCallback(ex);
+    });
+}
